44eeaefaba1e75d3acc432ee7e61009129626f18
SergonM <sa.gonzalezm123@uniandes.edu.co>
2023-10-17 16:08:08 -0500

    feat: tarjeta de informacion de series

--- main.ts
+++ main.ts
@@ -2,9 +2,59 @@ import {Serie} from './serie.js';
 import {Data} from './data.js';
 
 const seriesTbody: HTMLElement = document.getElementById('series')!;
+const tablaDatos: HTMLElement = document.getElementById("tabla-datos")!;
+const containerCard: HTMLElement = document.getElementById("container-card")!;
 let averageSeasons: number = 0;
 
 renderSeriesInTable(Data);
+
+tablaDatos.addEventListener("click", function(event){
+    const activeElement = event.target as HTMLElement;
+
+    if (activeElement.tagName == "TH" || activeElement.tagName == "TD"){
+        const fila = activeElement.parentElement as HTMLTableRowElement;
+        const id = fila.cells[0].textContent;
+
+        console.log(`Fila ${id} seleccionada`);
+
+        Data.forEach((serie) => {
+            if (id !== null && parseInt(id) === serie.num){
+                let selectedSerie: Serie = serie;
+                mostrarSerie(selectedSerie);
+            }
+        });
+    }
+});
+
+
+function mostrarSerie(selectedSerie: Serie){
+    // Se define el estilo de la tarjeta
+    let card = document.createElement("div");
+    card.className = "card";
+    card.style.width = "18rem";
+
+    // Se define el contenido de la tarjeta
+    card.innerHTML = `<img class="card-img-top" src="${selectedSerie.img}" alt="Img">`
+
+    // Creacion del card body
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    // Creacion del contenido del body
+    cardBody.innerHTML =   `<h5 class="card-title" style="font-weight: bold;">${selectedSerie.name}</h5>
+                            <p class="card-text">${selectedSerie.description}</p>
+                            <a href="${selectedSerie.url}" target="_blank">${selectedSerie.url}</a>`
+
+    // Se añade el body a la tarjeta
+    card.appendChild(cardBody);
+
+    // Se limpia el contenedor
+    containerCard.innerHTML = "";
+
+    // Se añade la tarjeta al contenedor
+    containerCard.appendChild(card);
+}
+
 
 function renderSeriesInTable(series: Serie[]): void{
     console.log("Leyendo series...");
@@ -27,3 +77,4 @@ function renderSeriesInTable(series: Serie[]): void{
 
     console.log("Lectura terminada");
 }
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,42 @@
 import { Data } from './data.js';
 var seriesTbody = document.getElementById('series');
+var tablaDatos = document.getElementById("tabla-datos");
+var containerCard = document.getElementById("container-card");
 var averageSeasons = 0;
 renderSeriesInTable(Data);
+tablaDatos.addEventListener("click", function (event) {
+    var activeElement = event.target;
+    if (activeElement.tagName == "TH" || activeElement.tagName == "TD") {
+        var fila = activeElement.parentElement;
+        var id_1 = fila.cells[0].textContent;
+        console.log("Fila ".concat(id_1, " seleccionada"));
+        Data.forEach(function (serie) {
+            if (id_1 !== null && parseInt(id_1) === serie.num) {
+                var selectedSerie = serie;
+                mostrarSerie(selectedSerie);
+            }
+        });
+    }
+});
+function mostrarSerie(selectedSerie) {
+    // Se define el estilo de la tarjeta
+    var card = document.createElement("div");
+    card.className = "card";
+    card.style.width = "18rem";
+    // Se define el contenido de la tarjeta
+    card.innerHTML = "<img class=\"card-img-top\" src=\"".concat(selectedSerie.img, "\" alt=\"Img\">");
+    // Creacion del card body
+    var cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    // Creacion del contenido del body
+    cardBody.innerHTML = "<h5 class=\"card-title\" style=\"font-weight: bold;\">".concat(selectedSerie.name, "</h5>\n                            <p class=\"card-text\">").concat(selectedSerie.description, "</p>\n                            <a href=\"").concat(selectedSerie.url, "\" target=\"_blank\">").concat(selectedSerie.url, "</a>");
+    // Se añade el body a la tarjeta
+    card.appendChild(cardBody);
+    // Se limpia el contenedor
+    containerCard.innerHTML = "";
+    // Se añade la tarjeta al contenedor
+    containerCard.appendChild(card);
+}
 function renderSeriesInTable(series) {
     console.log("Leyendo series...");
     series.forEach(function (serie) {
